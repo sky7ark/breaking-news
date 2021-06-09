866d3e35f239c23175751965613fc4b9e0bdf6db
Kazi Salith Ur Rahman <salith.rahman107@gmail.com>
2021-06-09 21:02:11 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import News from './Components/News/News';
 
 function App() {
+  const styles = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    margin: '10px',
+    padding: '10px',
+    height: 'auto',
+    justifyContent: 'center',
+    gap: '10px'
+  }
+  const h = {
+    textAlign: 'center',
+    color: 'White',
+    backgroundColor: 'Black',
+    border: '3px solid black',
+  }
+  const [articles, setArticles] = useState([])
+  useEffect( () => {
+    const url = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=1e6ce21a301f458690f983559d725963'
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setArticles(data.articles))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={h}>Latest Breaking News</h1>
+      <div style = {styles}>
+      {
+        articles.map(article => <News article={article}></News>)
+      }
+      </div>
     </div>
   );
 }
---
+++ src/Components/News/News.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+   // border: '1px solid red',
+    margin: '5px',
+    padding: '5px',
+    //width: '300px',
+
+}
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+  });
+
+const News = (props) => {
+    const {title, description, urlToImage} = props.article;
+    const classes = useStyles();
+    const [seeMore, setSeeMore] = useState(false)
+    
+    return (
+        <div style={styles}>
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        image={urlToImage}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {title}
+                         </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            {seeMore?description: description.slice(0,100)}
+                            <a onClick={() => setSeeMore(!seeMore)}>{seeMore?<b>  Show less</b>:<b>  Read More</b>}</a>
+                         </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Share
+                   </Button>
+                    <Button size="small" color="primary">
+                        Learn More
+                   </Button>
+                </CardActions>
+            </Card>
+        </div>
+    );
+
+
+};
+
+export default News;
